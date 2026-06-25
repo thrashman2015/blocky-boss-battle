164bdc021d8076ff73c5786af3ebeb7e60295f61
thrashman2015 <xavierthrasher2015@gmail.com>
2026-06-24 18:14:04 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,284 @@
+namespace SpriteKind {
+    export const PlayerShot = SpriteKind.create()
+    export const LifeBar = SpriteKind.create()
+}
+function moveSpriteInTime (sprite: Sprite, x: number, y: number, t: number) {
+    globalX = x
+    globalY = y
+    dx = x - sprite.x
+    dy = y - sprite.y
+    sprite.setVelocity(dx / t, dy / t)
+}
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.PlayerShot, function (sprite, otherSprite) {
+    if (started) {
+        info.changeScoreBy(20)
+        bossLife += -1
+        music.playTone(208, music.beat(BeatFraction.Eighth))
+        lifeBarPic.fillRect(bossLife * 2, 0, 96 - bossLife * 2, 5, 15)
+        lifeBar.setImage(lifeBarPic)
+        if (bossLife <= 0) {
+            game.over(true)
+        } else if (bossLife % 12 == 0) {
+            preSetBossPosition(80, 30)
+        }
+    }
+    otherSprite.destroy()
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    controller.moveSprite(mySprite, 50, 50)
+})
+function spell1 () {
+    enemyShootAimingPlayer(boss, 90, 5)
+}
+function moveSpriteRandom (sprite: Sprite, yLowerBound: number, outerBound: number, v: number) {
+    moveSprite(sprite, randint(outerBound, scene.screenWidth() - outerBound), randint(outerBound, yLowerBound), v)
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    shootBulletFromSprite(mySprite, 200, -90)
+})
+function nonSpell1 () {
+    for (let index2 = 0; index2 <= MAX - 1; index2++) {
+        shootBulletFromSprite(boss, 60, 360 / MAX * index2 + offset)
+    }
+    offset += 13
+}
+function spell2 () {
+    for (let index = 0; index <= 4; index++) {
+        shootBulletFromSprite(boss, 60, offset + index * 30)
+    }
+    offset += 23
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    scene.cameraShake(3, 200)
+    music.playTone(139, music.beat(BeatFraction.Eighth))
+    otherSprite.destroy()
+})
+function preSetBossPosition (x: number, y: number) {
+    started = false
+    ready = false
+    offset = 0
+    moveSpriteInTime(boss, x, y, 1)
+}
+controller.B.onEvent(ControllerButtonEvent.Released, function () {
+    controller.moveSprite(mySprite)
+})
+function moveSpriteRandomFixedTime (sprite: Sprite, yLowerBound: number, outerBound: number, t: number) {
+    moveSpriteInTime(sprite, randint(outerBound, scene.screenWidth() - outerBound), randint(outerBound, yLowerBound), t)
+}
+function nonSpell2 () {
+    for (let index3 = 0; index3 <= MAX - 1; index3++) {
+        shootBulletFromSprite(boss, 60, 360 / MAX * index3 + offset)
+        shootBulletFromSprite(boss, 100, 360 / MAX * (index3 + 0.5) + offset)
+    }
+}
+function shootBulletFromSprite (sourceSprite: Sprite, speed: number, angle: number) {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, sourceSprite, speed * Math.cos(angle / 57.3), speed * Math.sin(angle / 57.3))
+    projectile.setFlag(SpriteFlag.AutoDestroy, true)
+    if (sourceSprite.kind() == SpriteKind.Player) {
+        projectile.setKind(SpriteKind.PlayerShot)
+        projectile.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . 5 4 4 5 . . . . . . 
+            . . . . . 5 4 2 2 4 5 . . . . . 
+            . . . . . 5 4 2 2 4 5 . . . . . 
+            . . . . . . 5 4 4 5 . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+    } else {
+        projectile.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . 9 6 6 9 . . . . . . 
+            . . . . . 9 6 8 8 6 9 . . . . . 
+            . . . . . 9 6 8 8 6 9 . . . . . 
+            . . . . . . 9 6 6 9 . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+    }
+}
+function moveSprite (sprite: Sprite, x: number, y: number, v: number) {
+    globalX = x
+    globalY = y
+    dx = x - sprite.x
+    dy = y - sprite.y
+    speed = Math.sqrt(dx * dx + dy * dy)
+    if (speed != 0) {
+        sprite.setVelocity(dx / speed * v, dy / speed * v)
+    }
+}
+function enemyShootAimingPlayer (sprite: Sprite, speed: number, spread: number) {
+    shootBulletFromSprite(sprite, speed, Math.atan2(mySprite.y - sprite.y, mySprite.x - sprite.x) * 57.3 + randint(0 - spread, spread))
+}
+let lifeBarProgress = 0
+let bossProgress = 0
+let speed = 0
+let projectile: Sprite = null
+let ready = false
+let started = false
+let dy = 0
+let dx = 0
+let globalY = 0
+let globalX = 0
+let MAX = 0
+let offset = 0
+let lifeBar: Sprite = null
+let lifeBarPic: Image = null
+let boss: Sprite = null
+let mySprite: Sprite = null
+let bossLife = 0
+bossLife = 48
+info.setLife(20)
+info.setScore(0)
+music.setVolume(20)
+mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f e e e e f f . . . . 
+    . . . f e e e f f e e e f . . . 
+    . . f f f f f 2 2 f f f f f . . 
+    . . f f e 2 e 2 2 e 2 e f f . . 
+    . . f e 2 f 2 f f 2 f 2 e f . . 
+    . . f f f 2 2 e e 2 2 f f f . . 
+    . f f e f 2 f e e f 2 f e f f . 
+    . f e e f f e e e e f e e e f . 
+    . . f e e e e e e e e e e f . . 
+    . . . f e e e e e e e e f . . . 
+    . . e 4 f f f f f f f f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+mySprite.setPosition(80, 105)
+mySprite.setFlag(SpriteFlag.StayInScreen, true)
+controller.moveSprite(mySprite)
+boss = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .......fb111111bf.......
+    ......fffcdb1bdffff.....
+    ....fc111cbfbfc111cf....
+    ....f1b1b1ffff1b1b1f....
+    ....fbfbffffffbfbfbf....
+    .........ffffff.........
+    ...........fff..........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Enemy)
+boss.setPosition(-16, -16)
+lifeBarPic = image.create(96, 5)
+lifeBar = sprites.create(lifeBarPic, SpriteKind.LifeBar)
+lifeBar.setPosition(80, 5)
+lifeBar.setFlag(SpriteFlag.Ghost, true)
+offset = 0
+MAX = 10
+let bossCanMove = true
+preSetBossPosition(80, 30)
+game.onUpdate(function () {
+    if (Math.abs(boss.x - globalX) + Math.abs(boss.y - globalY) <= 2) {
+        boss.setVelocity(0, 0)
+        if (!(ready)) {
+            bossProgress += 1
+            if (bossProgress == 2) {
+                bossCanMove = false
+            } else {
+                if (bossProgress == 2) {
+                    MAX = 8
+                }
+                bossCanMove = true
+            }
+        }
+        ready = true
+    }
+})
+game.onUpdateInterval(750, function () {
+    if (started) {
+        if (bossProgress == 3) {
+            nonSpell2()
+        }
+    }
+})
+game.onUpdateInterval(2500, function () {
+    if (started && bossCanMove) {
+        moveSpriteRandom(boss, 40, 8, 60)
+    }
+})
+game.onUpdateInterval(150, function () {
+    if (started) {
+        if (bossProgress == 2) {
+            spell1()
+        } else {
+            if (bossProgress == 4) {
+                spell2()
+            }
+        }
+    }
+})
+game.onUpdateInterval(500, function () {
+    if (started) {
+        if (bossProgress == 1) {
+            nonSpell1()
+        }
+    }
+})
+game.onUpdateInterval(100, function () {
+    if (ready && !(started)) {
+        if (lifeBarProgress < 4) {
+            lifeBarPic.fillRect(24 * lifeBarProgress, 0, 24, 5, 14 - lifeBarProgress % 2 * 6)
+            lifeBarPic.fillRect(24 * lifeBarProgress, 1, 24, 3, lifeBarProgress % 2 * 5 + 4)
+            lifeBar.setImage(lifeBarPic)
+            lifeBarProgress += 1
+        } else {
+            started = true
+        }
+    }
+})
